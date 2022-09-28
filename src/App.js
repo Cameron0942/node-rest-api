9f31f04b6583e52505a1994d3af0de5b73df60c6
Cameron B <Cameron0942@users.noreply.github.com>
2022-09-28 15:36:52 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import Box from '@mui/material/Box';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{
+      display: 'flex',
+      textAlign: 'center',
+      width: 300,
+      height: 300,
+      backgroundColor: 'primary.dark',
+      '&:hover': {
+        backgroundColor: 'primary.main',
+        opacity: [0.9, 0.8, 0.7],
+      }
+    }} >
+      Here is some placeholder information
+    </Box>
   );
 }
 
