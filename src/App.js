c75440babd710f11560efad1f52ce2913756d3b9
Cameron B <Cameron0942@users.noreply.github.com>
2022-09-28 17:44:52 -0700

    prepared html body for JSON configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import Box from '@mui/material/Box';
+//import Box from '@mui/material/Box';
 import './App.css';
 
 function App() {
   return (
-    <Box sx={{
-      display: 'flex',
-      textAlign: 'center',
-      width: 300,
-      height: 300,
-      backgroundColor: 'primary.dark',
-      '&:hover': {
-        backgroundColor: 'primary.main',
-        opacity: [0.9, 0.8, 0.7],
-      }
-    }} >
-      Here is some placeholder information
-    </Box>
+    <p>What if this was a JSON object</p>
   );
 }
 
